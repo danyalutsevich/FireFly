5381fc0753fea68d21f288870aeacf08cd857b65
danyalutsevich <luchevich31@gmail.com>
2022-10-16 21:41:10 +0300

    variables, ratings css

--- src/Components/Ratings.jsx
+++ src/Components/Ratings.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
-
+import { React, useState } from 'react';
+import Links from './Variables';
+import "./Ratings.css";
 
 export default function Ratings() {
+
+  const [films, setFilms] = useState([])
+
+  if (films.length === 0) {
+    fetch(Links.top250).then(data => data.json()).then(data => setFilms(data.items))
+    return (<div>Loading...</div>)
+  }
+  
+
   return (
-  <div>
-    <h2>Ratings</h2>
-  </div>
+    <div>
+      <h1>Top 250</h1>
+      <table className="Table">
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Title</th>
+            <th>Rating</th>
+          </tr>
+        </thead>
+        <tbody>
+          {films.map((film, index) => {
+            return (
+              <tr key={film.id}>
+                <td>{index + 1}</td>
+                <td>{film.title}</td>
+                <td>{film.rating}</td>
+              </tr>
+            )
+          })}
+
+        </tbody>
+
+      </table>
+    </div>
   );
 }
---
+++ src/Components/Variables.js
@@ -0,0 +1,5 @@
+const Links = {
+    top250:"https://imdb-api.com/API/Top250Movies/k_9wcbl0rt",
+    search:"https://imdb-api.com/en/API/Search/k_9wcbl0rt/"
+}
+export default Links
