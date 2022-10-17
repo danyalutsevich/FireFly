ba11a9f1778139035c4818238a847b629e17d159
Daniil Klementiev <klementevdaniil@gmail.com>
2022-10-17 15:18:29 +0400

    homepage

--- src/Components/Variables.js
+++ src/Components/Variables.js
@@ -1,5 +1,6 @@
 const Links = {
     top250:"https://imdb-api.com/API/Top250Movies/k_9wcbl0rt",
-    search:"https://imdb-api.com/en/API/Search/k_9wcbl0rt/"
+    search:"https://imdb-api.com/en/API/Search/k_9wcbl0rt/",
+    trendings:"https://imdb-api.com/API/Top250Movies/k_3e9gj61r"
 }
 export default Links
