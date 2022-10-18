00be373211a0f0dd83b6a74049c6f95b94a031b5
grynch <nikita.grynch@gmail.com>
2022-10-19 00:12:46 +0300

    LikedPage

--- src/Components/Liked.jsx
+++ src/Components/Liked.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 
+import "./Liked.css";
+
 
 export default function Liked() {
   return (
   <div>
-    <h2>Liked</h2>
+    <div className="LikedHeader">
+      <h2 className="PageTitle">Liked</h2>
+      <div className="Sort">
+        
+        <button className="SortIconBtn">
+        <h3 className="SortElement">sort</h3>
+          <i class="material-icons SortElement">sort</i>
+        </button>
+        <div className="DropdownSortMethods">
+          <button className="SortMethodElement">by date added<i class="material-icons">arrow_upward</i></button>
+          <button className="SortMethodElement">by date added<i class="material-icons">arrow_downward</i></button>
+          <button className="SortMethodElement">by rating<i class="material-icons">arrow_upward</i></button>
+          <button className="SortMethodElement">by rating<i class="material-icons">arrow_downward</i></button>
+
+        </div>
+      </div>
+    </div>
+    <div className="Divider"></div>
   </div>
   );
 }
---
+++ src/Components/Liked/LikedContentCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import "./LikedContentCard.css";
+
+export default function LikedContentCard(props) {
+
+    return (<div>
+
+        <div>
+            
+        </div>
+
+    </div>);
+}
