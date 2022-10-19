da94feaa4f0a1a511a2698479f04ec337749c16a
Daniil Klementiev <klementevdaniil@gmail.com>
2022-10-19 22:17:41 +0400

    как вызвать 1 элемент films?

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,52 +1,72 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Links, { MovieDBLinks } from "./Variables";
 import "./Home.css";
 import Loading from "./Loading.jsx";
 
-export default class Film extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      movies: [],
-    };
+export default function Home() {
+  const [films, setFilms] = useState([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    fetch(MovieDBLinks.top_rated(page))
+      .then((data) => data.json())
+      .then((data) => setFilms(data.results));
+  }, [page]);
+
+  if (films === undefined) {
+    return <Loading />;
   }
 
-  componentDidMount() {
-    /* здесь надо фиксить, много нового, нихуя не понятно. */
-  }
+  return (
+    <div className="Ratings">
+      <a href="/error404">Top Rated</a>
 
-  render() {
-    const { error, isLoaded, movies } = this.state;
-    if (error) {
-      return (
-        <div className="ErrorMessage">
-          <p>Error: {error.message}</p>
+      <div className="TopFilm">
+        <img
+          className="MoviePoster"
+          src={MovieDBLinks.image + films[0].poster_path}
+          alt="movie poster"
+        ></img>
+        <div className="TopFilmInfo">
+          <h1>{films[0].original_title}</h1>
+          <p>{films[0].overview}</p>
         </div>
-      );
-    } else if (isLoaded != true) {
-      return <Loading />;
-    } else {
-      return (
-        <div className="Film">
-          <h1>Most popular movies</h1>
-          <ul>
-            {movies.map((movie, index) => (
-              <ol className="List">
-                <li key={movie.title}>
-                  <div className="rectangle">
-                    <a href="3">
-                      {movie.title} {movie.year} {movie.imDbRating}
-                    </a>
-                    <img src={movie.image} alt={movie.title} />
-                  </div>
-                </li>
-              </ol>
-            ))}
-          </ul>
-        </div>
-      );
-    }
-  }
+      </div>
+
+      <table className="Table">
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Picture</th>
+            <th>Title</th>
+            <th>Rating</th>
+          </tr>
+        </thead>
+        <tbody>
+          {films.map((film, index) => {
+            return (
+              <tr key={film.id}>
+                <td>{index + 1 + (page - 1) * films.length}</td>
+                <td className="imageCell">
+                  <img
+                    className="MoviePoster"
+                    src={MovieDBLinks.image + film.poster_path}
+                    alt={index}
+                  />
+                </td>
+                <td>{film.original_title}</td>
+                <td>{film.vote_average}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div>
+        <button onClick={() => (page > 1 ? setPage(page - 1) : null)}>
+          Previous
+        </button>
+        <button onClick={() => setPage(page + 1)}>Next</button>
+      </div>
+    </div>
+  );
 }
