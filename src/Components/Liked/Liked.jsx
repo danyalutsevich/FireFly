e453d3c0a6bc6d7297bc80415185928f1e16e631
danyalutsevich <luchevich31@gmail.com>
2022-10-21 22:17:32 +0300

    liked page merged

--- src/Components/Liked/Liked.jsx
+++ src/Components/Liked/Liked.jsx
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import {MovieDBLinks} from '../../Variables';
 
 import LikedCSS from "./Liked.module.scss";
 
@@ -8,6 +9,18 @@ export function Liked() {
   const [sortMethod, setSortMethod] = useState("by date added")
   const [icon, setIcon] = useState("arrow_upward")
 
+  const [likedFilms, setLikedFilms] = useState([])
+  const [page, setPage] = useState(1)
+
+  useEffect(() => {
+    fetch(MovieDBLinks.top_rated(page)).then(data => data.json()).then(data => setLikedFilms(data.results))
+    console.log(LikedCSS)
+  }, [page])
+
+  if (likedFilms.length === 0) {
+    return (<div>Loading...</div>)
+  }
+
   return (
   <div>
     <div className={LikedCSS.LikedHeader}>
@@ -15,20 +28,29 @@ export function Liked() {
       <div className={LikedCSS.Sort}>
         
         <button className={LikedCSS.SortIconBtn}>
-          <p className={LikedCSS.SortElement}>{sortMethod}<i class="material-icons">{icon}</i></p>
-          <i class="material-icons SortIcon">sort</i>
+          <p className={LikedCSS.SortElement}>{sortMethod}<i className="material-icons">{icon}</i></p>
+          <i className={`material-icons ${LikedCSS.SortIcon}`}>sort</i>
         </button>
         <div className={LikedCSS.DropdownSortMethods}>
-          <button className={LikedCSS.SortMethodElement} onClick={() => {setSortMethod("by date added"); setIcon("arrow_upward")}}>by date added<i class="material-icons">arrow_upward</i></button>
-          <button className={LikedCSS.SortMethodElement} onClick={() => {setSortMethod("by date added"); setIcon("arrow_downward")}}>by date added<i class="material-icons">arrow_downward</i></button>
-          <button className={LikedCSS.SortMethodElement} onClick={() => {setSortMethod("by rating"); setIcon("arrow_upward")}}>by rating<i class="material-icons">arrow_upward</i></button>
-          <button className={LikedCSS.SortMethodElement} onClick={() => {setSortMethod("by rating"); setIcon("arrow_downward")}}>by rating<i class="material-icons">arrow_downward</i></button>
+          <button className={LikedCSS.SortMethodElement} onClick={() => {setSortMethod("by date added"); setIcon("arrow_upward")}}>by date added<i className="material-icons">arrow_upward</i></button>
+          <button className={LikedCSS.SortMethodElement} onClick={() => {setSortMethod("by date added"); setIcon("arrow_downward")}}>by date added<i className="material-icons">arrow_downward</i></button>
+          <button className={LikedCSS.SortMethodElement} onClick={() => {setSortMethod("by rating"); setIcon("arrow_upward")}}>by rating<i className="material-icons">arrow_upward</i></button>
+          <button className={LikedCSS.SortMethodElement} onClick={() => {setSortMethod("by rating"); setIcon("arrow_downward")}}>by rating<i className="material-icons">arrow_downward</i></button>
 
         </div>
       </div>
     </div>
     <div className={LikedCSS.Divider}></div>
-    <LikedContentCard title = "Hello"/>
+    <div className={LikedCSS["liked-content"]}>
+    {
+      likedFilms.map((item) => {
+        return(
+        <LikedContentCard key = {item.id} imgSrc = {MovieDBLinks.image + item.poster_path}/>
+        )
+      })
+    }
+    </div>
+
   </div>
   );
 }
--- src/Components/LikedContentCard/LikedContentCard.jsx
+++ src/Components/LikedContentCard/LikedContentCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import LikedContentCardCSS from "./LikedContentCard.module.scss";
 
@@ -6,8 +6,18 @@ export function LikedContentCard(props) {
 
     return (<div>
 
-        <div>
-            <h1>{props.title}</h1>
+        <div className={LikedContentCardCSS["content-card"]} key={props.key}>
+            <img className={LikedContentCardCSS["liked-film-img"]} src={props.imgSrc} alt={props.key}></img>
+            <div className={LikedContentCardCSS["additional-info"]}>
+                <div className={LikedContentCardCSS["more-info"]}>
+                    <button className={LikedContentCardCSS["more-info-button"]}><i className="material-icons more_horiz-icon">more_horiz</i></button>
+                    <div className={LikedContentCardCSS["dropdown-more-info"]}>
+                        <button className={LikedContentCardCSS["more-info-element"]}>Перейти куда-то</button>
+                        <button className={LikedContentCardCSS["more-info-element"]}>Перейти куда-то еще</button>
+                    </div>
+                </div>
+                <button className={LikedContentCardCSS["save-button"]}><i className={`material-icons ${LikedContentCardCSS["heart-icon"]}`}>favorite</i></button>
+            </div>
         </div>
 
     </div>);
