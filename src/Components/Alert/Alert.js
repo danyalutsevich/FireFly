8b7874011be56127cfee193a0c08a31e73c24553
danyalutsevich <luchevich31@gmail.com>
2022-11-08 21:58:35 +0200

    build

--- src/Components/Alert/Alert.js
+++ src/Components/Alert/Alert.js
@@ -1,6 +1,7 @@
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 import "./Alert.css";
+
 export function Alert({
   title,
   text = "Please, try again!",
