18f7d7d18dbeff98d784f6ae69fce79d33878414
grynch <nikita.grynch@gmail.com>
2022-11-09 11:01:30 +0200

    added like, rate, add to watchlist in movie page

--- src/Components/Movie/Movie.jsx
+++ src/Components/Movie/Movie.jsx
@@ -1,9 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 
 import { MovieDBLinks } from "../../Variables";
 import { Loading } from "../Loading"
 import { Cast } from "./Cast";
+
+import { TableOperations } from "../Table/TableOperations";
+import { like, FirebaseContext, saveToWatchlist } from "../../firebase-config";
 
 
 import MovieCSS from "./Movie.module.scss";
@@ -13,11 +16,26 @@ export function Movie(props) {
     const [movie, setMovie] = useState(undefined);
     const { id } = useParams();
 
+    const contextData = useContext(FirebaseContext);
+    const [liked, setLiked] = useState([]);
+    const [watchlist, setWatchlist] = useState([]);
+    const [user, setUser] = useState(null);
+    const [ratings, setRatings] = useState([]);
+
+    
+
     useEffect(() => {
         if (id) {
             fetch(MovieDBLinks.movie(id)).then(data => data.json()).then(data => setMovie(data))
         }
     }, [id])
+
+    useEffect(() => {
+        setLiked(contextData.liked)
+        setWatchlist(contextData.watchlist)
+        setUser(contextData.user)
+        setRatings(contextData.ratings)
+      }, [contextData]);
 
     if (movie === undefined) {
         return <Loading />
@@ -37,6 +55,7 @@ export function Movie(props) {
                         movie.poster_path ? 
                         <img src={MovieDBLinks.image_original + movie.poster_path} alt="poster"></img>:
                         <img src={"/default_userpic.png"} alt="poster"></img>
+                        
                     }
                 </div>
                 <div>
@@ -45,6 +64,13 @@ export function Movie(props) {
                         <h3>{movie.release_date.slice(0, 4)}</h3>
                         <h2>Rating: {movie.vote_average}</h2>
                     </div>
+                    <div>
+                    {user ? <TableOperations filmID={id}
+                    liked={liked?.includes(Number(id))}
+                    watchlist={watchlist?.includes(Number(id))}
+                    rating={ratings[id]}
+                  /> : null}
+                  </div>
                     <div className={MovieCSS.About}>
                         <h2>{movie.tagline}</h2>
                         <h2>{movie.overview}</h2>
