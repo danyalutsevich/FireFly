4a056af0e86564070c94038f3985c991d2b3b794
Daniil <daniil@MacBook-Air-Daniil.local>
2022-11-16 19:52:51 +0400

    dropdown list for profile

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { NavLink, Link } from "react-router-dom";
-
 import HeaderCSS from "./Header.module.scss";
-import { FirebaseContext } from "../../firebase-config";
-
+import { FirebaseContext, logout } from "../../firebase-config";
 import { SearchResults } from "../SearchResults";
 
 export function Header(props) {
@@ -29,9 +27,13 @@ export function Header(props) {
             name="searchInput"
             className={HeaderCSS.searchInput}
             value={searchValue}
-            autoComplete='off'
-            onFocus={() => {setInputIsFocused(true)}}
-            onBlur={() => {setTimeout(() =>setInputIsFocused(false), 200)}}
+            autoComplete="off"
+            onFocus={() => {
+              setInputIsFocused(true);
+            }}
+            onBlur={() => {
+              setTimeout(() => setInputIsFocused(false), 200);
+            }}
             onChange={(e) => setSearchValue(e.target.value)}
             onKeyDown={(e) => {
               if (e.key === "Enter" && searchValue !== "") {
@@ -39,23 +41,71 @@ export function Header(props) {
               }
             }}
           ></input>
-          <NavLink className={HeaderCSS.MenuItem} to={searchValue === "" ? "#" : `/search/${searchValue}/1`}>
+          <NavLink
+            className={HeaderCSS.MenuItem}
+            to={searchValue === "" ? "#" : `/search/${searchValue}/1`}
+          >
             <i className="material-icons">search</i>
           </NavLink>
         </div>
-        <SearchResults searchValue={searchValue} isActive={inputIsFocused}></SearchResults>
+        <SearchResults
+          searchValue={searchValue}
+          isActive={inputIsFocused}
+        ></SearchResults>
       </div>
-      <NavLink className={HeaderCSS.MenuItem} to="/ratings/1">Ratings</NavLink>
-      {user ? <NavLink className={HeaderCSS.MenuItem} to="/liked">Liked</NavLink> : null}
-      {user ? <NavLink className={HeaderCSS.MenuItem} to="/watchlist">WatchList</NavLink> : null}
+      <NavLink className={HeaderCSS.MenuItem} to="/ratings/1">
+        Ratings
+      </NavLink>
       {user ? (
-        <NavLink to="/profile" className={HeaderCSS.Profile}>
-          {user.displayName}
-          <object data={user.photoURL || "https://"} type="image/jpg">
-            <img src={"/defaultUserPic.svg"} alt="Profile" />
-          </object>
+        <NavLink className={HeaderCSS.MenuItem} to="/liked">
+          Liked
         </NavLink>
+      ) : null}
+      {user ? (
+        <NavLink className={HeaderCSS.MenuItem} to="/watchlist">
+          WatchList
+        </NavLink>
+      ) : null}
+      {user ? (
+        <div className={HeaderCSS.Dropdown}>
+          <button>
+            <div className={HeaderCSS.Profile}>
+              <object data={user.photoURL || "https://"} type="image/jpg">
+                <img src={"/defaultUserPic.svg"} alt="Profile" />
+              </object>
+            </div>
+          </button>
+          <div className={HeaderCSS.DropdownContent}>
+            <a>
+              Signed in as <b>{user.displayName}</b>
+            </a>
+            <a href="/profile">Profile</a>
+            <a href="#" onClick={logout}>
+              Logout
+            </a>
+          </div>
+        </div>
       ) : (
+        // <div className={HeaderCSS.Profile}>
+        //   <button className={HeaderCSS.DropDownBtn}>
+        //     {user.displayName}{" "}
+        //     <object data={user.photoURL || "https://"} type="image/jpg">
+        //       // <img src={"/defaultUserPic.svg"} alt="Profile" />
+        //       //{" "}
+        //     </object>
+        //   </button>
+        //   <div class={HeaderCSS.DropDownContent}>
+        //     <a href="#">Ссылка 1</a>
+        //     <a href="#">Ссылка 2</a>
+        //     <a href="#">Ссылка 3</a>
+        //   </div>
+        // </div>
+        // <NavLink to="/profile" className={HeaderCSS.Profile}>
+        //   {user.displayName}
+        //   <object data={user.photoURL || "https://"} type="image/jpg">
+        //     <img src={"/defaultUserPic.svg"} alt="Profile" />
+        //   </object>
+        // </NavLink>
         <div className={HeaderCSS.Auth}>
           <NavLink to="/signin" className={HeaderCSS.SignIn}>
             SignIn
