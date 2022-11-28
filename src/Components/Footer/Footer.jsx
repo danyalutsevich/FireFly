94a56a30e2badd26a376b886f05cb7c20d8ead63
Daniil <daniil@MacBook-Air-Daniil.local>
2022-11-28 23:18:36 +0400

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -13,12 +13,13 @@ export function Footer() {
           <a href="https://github.com/danyalutsevich/FireFly">GitHub</a>&nbsp;
         </p>
         <p className={FooterCSS.Link}>
-          <a>LinkedIn</a>
-        </p>
-        <p className={FooterCSS.Link}>
           <a href="https://send.monobank.ua/jar/7c9hzdU5Q9">Support Us</a>
         </p>
       </div>
+      <p className={FooterCSS.Issue}>
+        <span>You can report a bug by opening an issue on our github</span>
+      </p>
+      <br />
       <div className={FooterCSS.FireFly}>
         <p>
           <span>© {new Date().getFullYear()}</span> FireFly
