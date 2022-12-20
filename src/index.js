0a28dd4afb9997f74ed795dc4b413fdf8dc2b15b
danyalutsevich <luchevich31@gmail.com>
2022-12-20 11:44:40 +0200

    let it snow, let it snow, let it snow

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App }from './Components/App';
+import Snowfall from 'react-snowfall'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Snowfall style={{position:"fixed", zIndex:"1000"}} speed={[1,5]} radius={[0.2,0.5]} wind={[-3,3]} snowflakeCount={200}/>
     <App />
   </React.StrictMode>
 );
