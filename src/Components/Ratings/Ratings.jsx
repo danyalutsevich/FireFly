61bee8a25ced5ec8ef22b0aaf45f9f6b1143b8a7
danyalutsevich <luchevich31@gmail.com>
2023-02-18 23:05:59 +0200

    schema org json ld, React Helmet

--- src/Components/Ratings/Ratings.jsx
+++ src/Components/Ratings/Ratings.jsx
@@ -4,6 +4,7 @@ import { NavLink, useParams } from "react-router-dom";
 import { Loading } from "../Loading";
 import { Table } from "../Table";
 import RatingsCSS from "./Ratings.module.scss";
+import { Helmet } from "react-helmet";
 
 export function Ratings() {
   const [films, setFilms] = useState([]);
@@ -13,10 +14,10 @@ export function Ratings() {
     fetch(MovieDBLinks.top_rated(page))
       .then((data) => data.json())
       .then((data) => setFilms(data.results));
-      document.title = "Ratings"
-      document.querySelector('meta[name="description"]').setAttribute("content", `Here you can find top rated films`)
-      document.querySelector('meta[property="og:description"]').setAttribute("content", `Here you can find top rated films`)
-      document.querySelector('meta[property="og:title"]').setAttribute("content", "Ratings")
+    document.title = "Ratings"
+    document.querySelector('meta[name="description"]').setAttribute("content", `Here you can find top rated films`)
+    document.querySelector('meta[property="og:description"]').setAttribute("content", `Here you can find top rated films`)
+    document.querySelector('meta[property="og:title"]').setAttribute("content", "Ratings")
   }, [page]);
 
   if (films.length === 0) {
@@ -24,6 +25,26 @@ export function Ratings() {
   }
 
   return (
-    <Table url={"ratings"} page={page} films={films} media_type={"movie"} />
+    <div>
+      <Helmet>
+        <title>{`Ratings (${page})`}</title>
+
+        <link rel="canonical" href={window.location.href} />
+        <meta property="og:title" content={`Ratings (${page})`} />
+        <meta property="og:description" content={`Here you can find top rated movied`} />
+        <meta property="og:image" content="/logo512.png" />
+        <meta property="og:url" content={window.location.href} />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Firefly" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={`Ratings, page ${page}`} />
+        <meta name="twitter:description" content={`Here you can find top rated movied`} />
+        <meta name="twitter:image" content={"/logo512.png"} />
+
+        <meta name="description" content={`Here you can find top rated movied`} />
+      </Helmet>
+      <Table url={"ratings"} page={page} films={films} media_type={"movie"} />
+    </div>
   )
 }
